ab56227730d64adbef7dd37fb78f26728b2be646
Borbely Hunor Marton <bor.hunor@gmail.com>
2017-05-22 23:17:31 +0200

    May update

--- src/data/portfolioItems.js
+++ src/data/portfolioItems.js
@@ -3,6 +3,18 @@ import * as techs from '../constants/techTypes';
 
 export default [
   {
+    title: 'Task Manager', type: types.CODEPEN, id: 'qmJvvY',
+    link: 'http://codepen.io/HunorMarton/full/qmJvvY/',
+    img: 'TaskManager', date: "May 2017",
+    techs: [techs.JAVASCRIPT, techs.REACT, techs.REDUX, techs.RXJS]
+  },
+  {
+    title: 'Luminosity', type: types.CODEPEN, id: 'BRRLPP',
+    link: 'http://codepen.io/HunorMarton/full/BRRLPP/',
+    img: 'Luminosity', date: "May 2017",
+    techs: [techs.JAVASCRIPT, techs.REACT, techs.CSS3, techs.SVG]
+  },
+  {
     title: 'Maze', type: types.GITHUB, id: 'maze',
     link: 'https://hunormarton.github.io/maze/',
     img: 'Maze', date: "April 2017",
@@ -22,61 +34,61 @@ export default [
   },
   {
     title: 'Square', type: types.CODEPEN, id: 'bqWxgp',
-    link: 'http://codepen.io/HunorMarton/pen/bqWxgp',
+    link: 'http://codepen.io/HunorMarton/full/bqWxgp',
     img: 'Square', date: 'March 2017',
     techs: [techs.PUG, techs.SCSS]
   },
   {
     title: 'Countdown', type: types.CODEPEN, id: 'mWWqBv',
-    link: 'http://codepen.io/HunorMarton/pen/mWWqBv',
+    link: 'http://codepen.io/HunorMarton/full/mWWqBv',
     img: 'Countdown', date: 'March 2017',
     techs: [techs.PUG, techs.CSS3, techs.SCSS]
   },
   {
     title: 'HSL Color Picker', type: types.CODEPEN, id: 'dvXVvQ',
-    link: 'http://codepen.io/HunorMarton/pen/dvXVvQ',
+    link: 'http://codepen.io/HunorMarton/full/dvXVvQ',
     img: 'HSL', date: 'March 2017',
     techs: [techs.JAVASCRIPT, techs.REACT, techs.REDUX, techs.RXJS, techs.SVG]
   },
   {
     title: 'RGB', type: types.CODEPEN, id: 'PpwRdM',
-    link: 'http://codepen.io/HunorMarton/pen/PpwRdM',
+    link: 'http://codepen.io/HunorMarton/full/PpwRdM',
     img: 'RGB', date: 'February 2017',
     techs: [techs.JAVASCRIPT, techs.JQUERY, 'mix-blend-mode', techs.PUG]
   },
   {
     title: 'Transpose of a matrix', type: types.CODEPEN, id: 'LxqwjX',
-    link: 'http://codepen.io/HunorMarton/pen/LxqwjX',
+    link: 'http://codepen.io/HunorMarton/full/LxqwjX',
     img: 'Transpose', date: 'February 2017',
     techs: [techs.PUG, techs.CSS3]
   },
   {
     title: 'Sine', type: types.CODEPEN, id: 'ggQGqQ',
-    link: 'http://codepen.io/HunorMarton/pen/ggQGqQ',
+    link: 'http://codepen.io/HunorMarton/full/ggQGqQ',
     img: 'Sine', date: 'February 2017',
     techs: [techs.JAVASCRIPT, techs.REACT, techs.SVG]
   },
   {
     title: 'Redux Undo', type: types.CODEPEN, id: 'wgpKGL',
-    link: 'http://codepen.io/HunorMarton/pen/wgpKGL',
+    link: 'http://codepen.io/HunorMarton/full/wgpKGL',
     img: 'Undo', date: 'February 2017',
     techs: [techs.JAVASCRIPT, techs.REACT, techs.REDUX]
   },
   {
     title: 'Text on the wall', type: types.CODEPEN, id: 'EZgMGb',
-    link: 'http://codepen.io/HunorMarton/pen/EZgMGb',
+    link: 'http://codepen.io/HunorMarton/full/EZgMGb',
     img: 'Wall', date: 'January 2017',
     techs: [techs.PUG, techs.CSS3]
   },
   {
     title: 'Rotating 3D figure', type: types.CODEPEN, id: 'mRbNQd',
-    link: 'http://codepen.io/HunorMarton/pen/mRbNQd',
+    link: 'http://codepen.io/HunorMarton/full/mRbNQd',
     img: 'Figure', date: 'January 2017',
     techs: [techs.PUG, techs.CSS3, techs.SCSS]
   },
   {
     title: 'Analog Text Clock', type: types.CODEPEN, id: 'vyGVpX',
-    link: 'http://codepen.io/HunorMarton/pen/vyGVpX',
+    link: 'http://codepen.io/HunorMarton/full/vyGVpX',
     img: 'Clock', date: 'November 2016',
     techs: [techs.JAVASCRIPT, techs.REACT]
   },
