7a58a1ebc78a7a1fe336c5bda582f62de7fa546f
hunormarton <bor.hunor@gmail.com>
2018-12-01 19:58:13 +0100

    New Build

---
+++ build/precache-manifest.4e227cd632012a2cb75bb09ca3bdf7f6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "b72dad615c0c8b729b0d",
+    "url": "/static/css/main.4b3d31b7.chunk.css"
+  },
+  {
+    "revision": "b72dad615c0c8b729b0d",
+    "url": "/static/js/main.b72dad61.chunk.js"
+  },
+  {
+    "revision": "247647a3ae7f9d201806",
+    "url": "/static/js/1.247647a3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73b837cc0111511f520dd704bdd861d3",
+    "url": "/static/media/Solitaire@2x-min.73b837cc.png"
+  },
+  {
+    "revision": "34aa089cfbf0fc6e598bd1e0c6b0cbc9",
+    "url": "/static/media/TaskManager@2x-min.34aa089c.png"
+  },
+  {
+    "revision": "4d93a260a1659f14a7fbd341a4ae4b5c",
+    "url": "/static/media/Luminosity-min.4d93a260.png"
+  },
+  {
+    "revision": "98ccd7105fce964bff840e60dbec3c91",
+    "url": "/static/media/Luminosity@2x-min.98ccd710.png"
+  },
+  {
+    "revision": "9959b5a7231bd97a53876e2860cd0145",
+    "url": "/static/media/Maze-min.9959b5a7.png"
+  },
+  {
+    "revision": "6f7f1f8c4b964d2943e1d0ea09c32e7a",
+    "url": "/static/media/Maze@2x-min.6f7f1f8c.png"
+  },
+  {
+    "revision": "f0a4c182aaeb46e369e6bb6f0571137a",
+    "url": "/static/media/Compass-min.f0a4c182.png"
+  },
+  {
+    "revision": "f141a78fe6f50151ce6119e34014b06d",
+    "url": "/static/media/Compass@2x-min.f141a78f.png"
+  },
+  {
+    "revision": "dea814530a75f04c2ba5c9eafbf95f69",
+    "url": "/static/media/SVGCurves-min.dea81453.png"
+  },
+  {
+    "revision": "58854b06db58a61adce9a2512436c96f",
+    "url": "/static/media/SVGCurves@2x-min.58854b06.png"
+  },
+  {
+    "revision": "a9ff379e913434d790fc45cb283940a8",
+    "url": "/static/media/HSL@2x-min.a9ff379e.png"
+  },
+  {
+    "revision": "81a580492b51e0a8b7febfecdcfc698d",
+    "url": "/static/media/RGB@2x-min.81a58049.png"
+  },
+  {
+    "revision": "2d10be0028ff35573b8c49ec444710af",
+    "url": "/static/media/Undo-min.2d10be00.png"
+  },
+  {
+    "revision": "41b90f8443393a260da3789f28a86ad8",
+    "url": "/static/media/Undo@2x-min.41b90f84.png"
+  },
+  {
+    "revision": "161e160009cbd54985a731895e9ce20c",
+    "url": "/static/media/Clock@2x-min.161e1600.png"
+  },
+  {
+    "revision": "8edbc92a4bd92c6f99439c54e81f24ac",
+    "url": "/index.html"
+  }
+];
