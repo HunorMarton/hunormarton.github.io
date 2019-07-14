2dbdb644d136f462651e6d4cf6166f21caebd730
Hunor <hunor@unumotors.com>
2019-07-15 00:04:13 +0200

    New Build

---
+++ precache-manifest.6ef2707e5d1b642b6cb29df96933871f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01b5188262d53e4c72eec929f06c0a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8f7d8a771d8d03a0c2",
+    "url": "/static/css/main.55b57a8d.chunk.css"
+  },
+  {
+    "revision": "a8f90f4e050342561f52",
+    "url": "/static/js/2.61fe039b.chunk.js"
+  },
+  {
+    "revision": "fe8f7d8a771d8d03a0c2",
+    "url": "/static/js/main.bfb0f428.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "161e160009cbd54985a731895e9ce20c",
+    "url": "/static/media/Clock@2x-min.161e1600.png"
+  },
+  {
+    "revision": "f0a4c182aaeb46e369e6bb6f0571137a",
+    "url": "/static/media/Compass-min.f0a4c182.png"
+  },
+  {
+    "revision": "f141a78fe6f50151ce6119e34014b06d",
+    "url": "/static/media/Compass@2x-min.f141a78f.png"
+  },
+  {
+    "revision": "cd5e783b74c503da2e592869db0003a8",
+    "url": "/static/media/CrossyRoad-min.cd5e783b.png"
+  },
+  {
+    "revision": "a3048a677067c740c743590f7b8e2d65",
+    "url": "/static/media/CrossyRoad@0.5x-min.a3048a67.png"
+  },
+  {
+    "revision": "a9ff379e913434d790fc45cb283940a8",
+    "url": "/static/media/HSL@2x-min.a9ff379e.png"
+  },
+  {
+    "revision": "4d93a260a1659f14a7fbd341a4ae4b5c",
+    "url": "/static/media/Luminosity-min.4d93a260.png"
+  },
+  {
+    "revision": "98ccd7105fce964bff840e60dbec3c91",
+    "url": "/static/media/Luminosity@2x-min.98ccd710.png"
+  },
+  {
+    "revision": "9959b5a7231bd97a53876e2860cd0145",
+    "url": "/static/media/Maze-min.9959b5a7.png"
+  },
+  {
+    "revision": "6f7f1f8c4b964d2943e1d0ea09c32e7a",
+    "url": "/static/media/Maze@2x-min.6f7f1f8c.png"
+  },
+  {
+    "revision": "81a580492b51e0a8b7febfecdcfc698d",
+    "url": "/static/media/RGB@2x-min.81a58049.png"
+  },
+  {
+    "revision": "dea814530a75f04c2ba5c9eafbf95f69",
+    "url": "/static/media/SVGCurves-min.dea81453.png"
+  },
+  {
+    "revision": "58854b06db58a61adce9a2512436c96f",
+    "url": "/static/media/SVGCurves@2x-min.58854b06.png"
+  },
+  {
+    "revision": "73b837cc0111511f520dd704bdd861d3",
+    "url": "/static/media/Solitaire@2x-min.73b837cc.png"
+  },
+  {
+    "revision": "34aa089cfbf0fc6e598bd1e0c6b0cbc9",
+    "url": "/static/media/TaskManager@2x-min.34aa089c.png"
+  },
+  {
+    "revision": "c1e3f35d5e3ad0b447bd342da601be9c",
+    "url": "/static/media/TicTacToe@2x.c1e3f35d.png"
+  },
+  {
+    "revision": "2d10be0028ff35573b8c49ec444710af",
+    "url": "/static/media/Undo-min.2d10be00.png"
+  },
+  {
+    "revision": "41b90f8443393a260da3789f28a86ad8",
+    "url": "/static/media/Undo@2x-min.41b90f84.png"
+  },
+  {
+    "revision": "17f524ec8505db09dff8146acd42d473",
+    "url": "/static/media/leo.17f524ec.png"
+  },
+  {
+    "revision": "369a135025d724abb4da0a1f76b83a8e",
+    "url": "/static/media/sap.369a1350.png"
+  },
+  {
+    "revision": "19a08a70c6ff74f49f6e4e093ef90d41",
+    "url": "/static/media/university.19a08a70.png"
+  },
+  {
+    "revision": "9a877915374baa03f2d8674b5c1e4443",
+    "url": "/static/media/up.9a877915.png"
+  }
+]);
