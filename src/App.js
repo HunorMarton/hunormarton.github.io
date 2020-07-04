45a2cfb3997ae81b1a402468b497720de00eb221
HunorMarton <bor.hunor@gmail.com>
2020-07-04 14:29:35 +0200

    User react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as techs from './constants/techTypes'
 import Navigation from './components/Navigation'
 import Header from './components/Header'
@@ -7,24 +7,18 @@ import Portfolio from './components/Portfolio'
 import Resume from './components/Resume'
 import Contact from './components/Contact'
 
-export default class Root extends React.Component {
-  state = {
-    filter: techs.ALL,
-  }
+export default function Root() {
+  const [filter, setFilter] = useState(techs.ALL)
 
-  setFilter = filter => this.setState({ filter })
+  return (
+    <div id="container">
+      <Navigation />
 
-  render() {
-    return (
-      <div id="container">
-        <Navigation />
-
-        <Header />
-        <Skills filter={this.state.filter} setFilter={this.setFilter} />
-        <Portfolio filter={this.state.filter} />
-        <Resume />
-        <Contact />
-      </div>
-    )
-  }
+      <Header />
+      <Skills filter={filter} setFilter={setFilter} />
+      <Portfolio filter={filter} />
+      <Resume />
+      <Contact />
+    </div>
+  )
 }
