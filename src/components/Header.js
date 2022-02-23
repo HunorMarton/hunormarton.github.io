85d566ce84f481d5c6b5a09f76fec266818882e2
HunorMarton <bor.hunor@gmail.com>
2022-02-24 00:17:02 +0100

    Change intro phrasing

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,8 @@ export default function Header() {
         Full-stack web developer focused on frontend with a deep understanding
         of React and Vue. Enthusiastic about UX design. I coach the next
         generation of web developers at codebar, share hobby projects on
-        CodePen, publish video tutorials on YouTube, and occasionally write
-        articles on CSS-Tricks and freeCodeCamp.
+        CodePen, publish tutorials on YouTube, and occasionally write articles
+        on freeCodeCamp and CSS-Tricks.
       </p>
     </section>
   )
