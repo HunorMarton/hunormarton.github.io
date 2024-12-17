d213a10293b3b3c4a51c7f207efea01f1a6707b8
Hunor Márton Borbély <bor.hunor@gmail.com>
2024-12-17 14:03:31 +0100

    Boilerplate for blog pages

---
+++ src/content/blog/intro-to-reinforcement-learning.md
@@ -0,0 +1,6 @@
+---
+title: Introduction to Reinforcement Learning
+date: 2024-12-17
+---
+
+Content
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,39 +1,47 @@
-
-import { z, defineCollection, type ImageFunction } from 'astro:content';
-import { SourceType, TechTypes } from '../types';
+import { z, defineCollection, type ImageFunction } from "astro:content";
+import { SourceType, TechTypes } from "../types";
 
 const hobbiesCollection = defineCollection({
-    type: 'content', // v2.5.0 and later
-    schema: ({ image }) => z.object({
-        title: z.string(),
-        highlight: z.boolean().optional(),
-        image: image().optional(),
-        desc: z.string().optional(),
-        date: z.date(),
-        sources: z.record(z.nativeEnum(SourceType), z.string()),
-        techs: z.array(z.union([
-            z.nativeEnum(TechTypes),
-            z.string()
-        ])),
+  type: "content", // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      highlight: z.boolean().optional(),
+      image: image().optional(),
+      desc: z.string().optional(),
+      date: z.date(),
+      sources: z.record(z.nativeEnum(SourceType), z.string()),
+      techs: z.array(z.union([z.nativeEnum(TechTypes), z.string()])),
     }),
 });
 
 const resumeItemCollection = defineCollection({
-    type: 'content',
-    schema: ({ image }: { image: ImageFunction }) => z.object({
-        header: z.string(),
-        subheader: z.string(),
-        pic: image(),
-        link: z.string().optional(),
-        location: z.string().optional(),
-        start: z.date(),
-        end: z.date(),
-    })
-})
+  type: "content",
+  schema: ({ image }: { image: ImageFunction }) =>
+    z.object({
+      header: z.string(),
+      subheader: z.string(),
+      pic: image(),
+      link: z.string().optional(),
+      location: z.string().optional(),
+      start: z.date(),
+      end: z.date(),
+    }),
+});
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: () =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+    }),
+});
 
 export const collections = {
-    'hobbies': hobbiesCollection,
-    education: resumeItemCollection,
-    'work': resumeItemCollection,
-    volunteering: resumeItemCollection
+  hobbies: hobbiesCollection,
+  education: resumeItemCollection,
+  work: resumeItemCollection,
+  volunteering: resumeItemCollection,
+  blog: blogCollection,
 };
